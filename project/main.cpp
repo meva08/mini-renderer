03584889ad4ca712d2ef9fa5546ee95b0b7b7133
Maryeva Gonzalez <144571162+meva08@users.noreply.github.com>
2026-08-11 14:20:14 -0500

    simple rasterization

--- project/main.cpp
+++ project/main.cpp
@@ -10,13 +10,15 @@ constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
 // globals
-constexpr int width  = 800;
-constexpr int height = 800;
+constexpr int width  = 128;
+constexpr int height = 128;
 
 void drawLine(int ax, int ay, int bx, int by, TGAImage &image, const TGAColor &color);
 void drawMesh(Mesh m, TGAImage &image);
 void drawTriangle(vertex v0, vertex v1, vertex v2, TGAImage &image);
 vertex project(vertex v);
+void triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &image, const TGAColor &color);
+float cross(int ax, int ay, int bx, int by, int px, int py);
 
 
 int main(int argc, char** argv) {
@@ -32,8 +34,9 @@ int main(int argc, char** argv) {
     Mesh m;
     m.readOBJ(argv[1]);
 
-    // render mesh data
-    drawMesh(m, framebuffer);
+    triangle(  7, 45, 35, 100, 45,  60, framebuffer, red);
+    triangle(120, 35, 90,   5, 45, 110, framebuffer, white);
+    triangle(115, 83, 80,  90, 85, 120, framebuffer, green);
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
@@ -85,7 +88,7 @@ void drawMesh(Mesh m, TGAImage &image)
 {
     int n = m.faces.size();
 
-    for (int i = 0; i < n - 3; i += 3)
+    for (int i = 0; i + 2 < n; i += 3)
     {
         drawTriangle(m.vertices[m.faces[i]], m.vertices[m.faces[i + 1]], m.vertices[m.faces[i + 2]], image);
     }
@@ -109,6 +112,50 @@ void drawTriangle(vertex v0, vertex v1, vertex v2, TGAImage &image)
     drawLine(vp2.x, vp2.y, vp0.x, vp0.y, image, blue);
 }
 
+void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &image, const TGAColor &color)
+{
+    if (ay > by) { std::swap(ax, bx); std::swap(ay, by); }
+    if (ay > cy) { std::swap(ax, cx); std::swap(ay, cy); }
+    if (by > cy) { std::swap(bx, cx); std::swap(by, cy); }
+
+    // find min/max values for bb
+    int minX = std::min({ax, bx, cx});
+    int maxX = std::max({ax, bx, cx});
+    int minY = std::min({ay, by, cy});
+    int maxY = std::max({ay, by, cy});
+
+    // find area
+    float area = cross(ax, ay, bx, by, cx, cy);
+
+    #pragma omp parallel for
+    for (int x = minX; x <= maxX; x++)
+    {
+        for (int y = minY; y <= maxY; y++)
+        {
+            // for each pixel
+
+            // get w values
+            float w0 = cross(ax, ay, bx, by, x, y);
+            float w1 = cross(bx, by, cx, cy, x, y);
+            float w2 = cross(cx, cy, ax, ay, x, y);
+
+            float alpha = w0 / area;
+            float beta = w1 / area;
+            float gamma = w2 / area;
+
+            if (alpha >= 0 && beta >= 0 && gamma >= 0)
+            {
+                image.set(x, y, color);
+            }
+        }
+    }
+
+    // draw outline
+    drawLine(ax, ay, bx, by, image, color);
+    drawLine(bx, by, cx, cy, image, color);
+    drawLine(cx, cy, ax, ay, image, color);
+}
+
 vertex project(vertex v)
 {
     // drop z coord
@@ -116,4 +163,16 @@ vertex project(vertex v)
     newV.x = (v.x + 1.0)  * width / 2;
     newV.y = (v.y + 1.0) * height / 2;
     return newV;
+}
+
+float cross(int ax, int ay, int bx, int by, int px, int py)
+{
+    // vec 1 = B  - A
+    float v1X = bx - ax;
+    float v1Y = by - ay;
+    // vec 2 = P - A
+    float v2X = px - ax;
+    float v2Y = py - ay;
+
+    return (v1X * v2Y) - (v1Y * v2X);
 }
